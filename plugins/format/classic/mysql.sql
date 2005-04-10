b8ae18088fb5167ae6b5b146b16a48163f2d992e
Nicolas Delon <nicolas.delon@prelude-ids.com>
2005-04-10 17:58:11 +0000

    bug fix: forgot to update the schema version

--- plugins/format/classic/mysql.sql
+++ plugins/format/classic/mysql.sql
@@ -4,7 +4,7 @@ CREATE TABLE _format (
  name VARCHAR(255) NOT NULL,
  version VARCHAR(255) NOT NULL
 );
-INSERT INTO _format (name, version) VALUES('classic', '14');
+INSERT INTO _format (name, version) VALUES('classic', '14.1');
 
 DROP TABLE IF EXISTS Prelude_Alert;
 
