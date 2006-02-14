ef916b944ddf48e6c072afbfe4941b3cf04ff0cf
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2006-02-14 01:28:35 +0000

    Include patch from Emmanuel Saracco <esaracco@free.fr> to fix PostgreSQL header path.

--- plugins/sql/pgsql/pgsql.c
+++ plugins/sql/pgsql/pgsql.c
@@ -36,7 +36,7 @@
 
 #include <libprelude/prelude-error.h>
 #include <libprelude/idmef.h>
-#include <libpq-fe.h>
+#include <postgresql/libpq-fe.h>
 
 #include "preludedb-sql-settings.h"
 #include "preludedb-sql.h"
