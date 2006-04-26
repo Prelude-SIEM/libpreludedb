d84f39edfed8a945fa5ffeeeaddf93387c53e5ec
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2006-04-26 09:48:30 +0000

    PostgreSQL typing fix.

--- plugins/format/classic/pgsql-update-14-5.sql
+++ plugins/format/classic/pgsql-update-14-5.sql
@@ -1,6 +1,9 @@
-UPDATE _format SET version="14.5";
+BEGIN;
 
+UPDATE _format SET version='14.5';
 ALTER TABLE Prelude_SnmpService DROP COLUMN community;
-ALTER TABLE Prelude_SnmpService ADD COLUMN message_processing_model INTEGER UNSIGNED NULL;
-ALTER TABLE Prelude_SnmpService ADD COLUMN security_model INTEGER UNSIGNED NULL;
-ALTER TABLE Prelude_SnmpService ADD COLUMN security_level INTEGER UNSIGNED NULL;
+ALTER TABLE Prelude_SnmpService ADD COLUMN message_processing_model INT8 NULL;
+ALTER TABLE Prelude_SnmpService ADD COLUMN security_model INT8 NULL;
+ALTER TABLE Prelude_SnmpService ADD COLUMN security_level INT8 NULL;
+
+COMMIT;
