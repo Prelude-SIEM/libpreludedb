73305d7b4529a1b4a8d004f9007d3e8bceec0bca
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-01-02 12:46:29 +0000

    Drop 'NOT NULL' constraint with a separate query (Fix #271). Thanks to Pierre Chifflier <p.chifflier@inl.fr> for pointing this out.

--- plugins/format/classic/pgsql-update-14-7.sql
+++ plugins/format/classic/pgsql-update-14-7.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 UPDATE _format SET version='14.7';
-ALTER TABLE Prelude_Impact ALTER COLUMN description TYPE TEXT NULL;
+ALTER TABLE Prelude_Impact ALTER COLUMN description TYPE TEXT;
+ALTER TABLE Prelude_Impact ALTER COLUMN description DROP NOT NULL;
 
 COMMIT;
