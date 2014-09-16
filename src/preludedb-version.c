8c4083e42c6bcd06f83751c0ad99269506491498
Yoann Vandoorselaere <yoannv@gmail.com>
2014-09-16 13:40:25 +0200

    Provide more version number information (major, minor, micro, level, serial)
    Generated preludedb-version.h header file now include separate major, minor,
    micro, serial, and level (read ahead), definition.
    
    The level information is used to tag a release as alpha, beta, rc, or final.
    We also provide a LIBPRELUDEDB_HEXVERSION definition, making it easy to add
    version checking in the code. For example, checking for libpreludedb higher
    than libpreludedb 1.2.5 would look like.
    
    Change-Id: Id878bd8265a23a9d6d4c3e1770724e5040c49be2

--- src/preludedb-version.c
+++ src/preludedb-version.c
@@ -21,36 +21,70 @@
 *
 *****/
 
+#include "config.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "preludedb-version.h"
-#include "config.h"
 
 
+
+static int levelstr_to_int(const char *wanted)
+{
+        int i;
+        struct {
+                int level;
+                const char *level_str;
+        } tbl[] = {
+                { LIBPRELUDEDB_RELEASE_LEVEL_ALPHA, "alpha" },
+                { LIBPRELUDEDB_RELEASE_LEVEL_BETA, "beta"   },
+                { LIBPRELUDEDB_RELEASE_LEVEL_RC, "rc"       }
+        };
+
+        for ( i = 0; i < sizeof(tbl) / sizeof(*tbl); i++ ) {
+                if ( strcmp(wanted, tbl[i].level_str) == 0 )
+                        return tbl[i].level;
+        }
+
+        return -1;
+}
+
+
+
+/**
+ * preludedb_check_version:
+ * @req_version: The minimum acceptable version number.
+ *
+ * If @req_version is NULL this function will return the version of the library.
+ * Otherwise, @req_version will be compared to the current library version. If
+ * the library version is higher or equal, this function will return the current
+ * library version. Otherwise, NULL is returned.
+ *
+ * Returns: The current library version, or NULL if @req_version doesn't match.
+ */
 const char *preludedb_check_version(const char *req_version)
 {
         int ret;
-        int major, minor, micro;
-        int rq_major, rq_minor, rq_micro;
+        unsigned int hexreq;
+        char rq_levels[6] = { 0 };
+        int rq_major, rq_minor, rq_micro, rq_level = 0, rq_patch = 0;
 
         if ( ! req_version )
                 return VERSION;
-        
-        ret = sscanf(VERSION, "%d.%d.%d", &major, &minor, &micro);
-        if ( ret != 3 )
+
+        ret = sscanf(req_version, "%d.%d.%d%5[^0-9]%d", &rq_major, &rq_minor, &rq_micro, rq_levels, &rq_patch);
+        if ( ret < 3 )
                 return NULL;
-        
-        ret = sscanf(req_version, "%d.%d.%d", &rq_major, &rq_minor, &rq_micro);
-        if ( ret != 3 )
-                return NULL;
-        
-        if ( major > rq_major
-             || (major == rq_major && minor > rq_minor)
-             || (major == rq_major && minor == rq_minor && micro > rq_micro)
-             || (major == rq_major && minor == rq_minor && micro == rq_micro) ) {
-                return VERSION;
+
+        if ( *rq_levels == 0 || *rq_levels == '.' )
+                rq_level = LIBPRELUDEDB_RELEASE_LEVEL_FINAL;
+        else {
+                rq_level = levelstr_to_int(rq_levels);
+                if ( rq_level < 0 )
+                        return NULL;
         }
 
-        return NULL;
+        hexreq = (rq_major << 24) | (rq_minor << 16) | (rq_micro << 8) | (rq_level << 4) | (rq_patch << 0);
+        return (hexreq <= LIBPRELUDEDB_HEXVERSION) ? VERSION : NULL;
 }
