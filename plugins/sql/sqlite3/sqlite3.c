b0a21bc8aadb58b3b3f118261742d457d188c53f
Antoine LUONG <antoine.luong@c-s.fr>
2020-07-30 20:00:10 +0200

    [#4084] Change company name
    Change-Id: Ic1719ac5d3151b94495001b8abd447318e0022c3

--- plugins/sql/sqlite3/sqlite3.c
+++ plugins/sql/sqlite3/sqlite3.c
@@ -1,6 +1,6 @@
 /*****
  *
- * Copyright (C) 2005-2020 CS-SI. All Rights Reserved.
+ * Copyright (C) 2005-2020 CS GROUP - France. All Rights Reserved.
  * Author: Rob Holland <rob@inversepath.com>
  *
  * This file is part of the PreludeDB library.
